598ec4fda91b5fc02a95d7165ec2aad7aaf3e8bd
Vladyslav Pasichniuk <vladyslav_pasichniuk@epam.com>
2026-01-21 15:28:08 +0200

    feat: add swagger endpoints

---
+++ src/main/java/com/epam/aidial/deployment/manager/configuration/SwaggerConfiguration.java
@@ -0,0 +1,14 @@
+package com.epam.aidial.deployment.manager.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI();
+    }
+}
