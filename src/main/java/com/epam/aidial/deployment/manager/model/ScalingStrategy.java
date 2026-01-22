940bc99f59a5b33b54a63d691ef9281fad8ae5d1
Oleksii Donets <oleksii_donets@epam.com>
2026-01-22 18:26:15 +0200

    feat: update inference api to support scaling

---
+++ src/main/java/com/epam/aidial/deployment/manager/model/ScalingStrategy.java
@@ -0,0 +1,15 @@
+package com.epam.aidial.deployment.manager.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScalingStrategy {
+    @JsonProperty("$type")
+    private ScalingStrategyType type;
+    private double threshold;
+}
---
+++ src/main/java/com/epam/aidial/deployment/manager/web/dto/ScalingStrategyDto.java
@@ -0,0 +1,17 @@
+package com.epam.aidial.deployment.manager.web.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScalingStrategyDto {
+    @NotNull
+    @JsonProperty("$type")
+    private ScalingStrategyTypeDto type;
+    private double threshold;
+}
