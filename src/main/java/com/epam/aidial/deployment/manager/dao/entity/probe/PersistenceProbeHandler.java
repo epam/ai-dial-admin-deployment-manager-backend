cefd8c51cfad5222d920999464766e1d87a42cae
Vladyslav Pasichniuk <vladyslav_pasichniuk@epam.com>
2026-02-03 18:25:15 +0200

    Fix probe handler type

--- src/main/java/com/epam/aidial/deployment/manager/dao/entity/probe/PersistenceProbeHandler.java
+++ src/main/java/com/epam/aidial/deployment/manager/dao/entity/probe/PersistenceProbeHandler.java
@@ -3,7 +3,7 @@ package com.epam.aidial.deployment.manager.dao.entity.probe;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", visible = true)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "$type")
 @JsonSubTypes({
         @JsonSubTypes.Type(value = PersistenceHttpGetProbe.class, name = "httpGet"),
         @JsonSubTypes.Type(value = PersistenceTcpSocketProbe.class, name = "tcpSocket"),
--- src/main/java/com/epam/aidial/deployment/manager/web/dto/probe/ProbeHandlerDto.java
+++ src/main/java/com/epam/aidial/deployment/manager/web/dto/probe/ProbeHandlerDto.java
@@ -3,7 +3,7 @@ package com.epam.aidial.deployment.manager.web.dto.probe;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", visible = true)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "$type", visible = true)
 @JsonSubTypes({
         @JsonSubTypes.Type(value = HttpGetProbeDto.class, name = "httpGet"),
         @JsonSubTypes.Type(value = TcpSocketProbeDto.class, name = "tcpSocket"),
