fd63f72f942f13a2cd034cdb9ac3b045680c54ff
Vladyslav Pasichniuk <62588934+Pasichniuk@users.noreply.github.com>
2026-02-17 10:12:22 +0200

    feat: support TCP socket startup probe (#143)

--- src/main/java/com/epam/aidial/deployment/manager/web/dto/probe/ProbeHandlerDto.java
+++ src/main/java/com/epam/aidial/deployment/manager/web/dto/probe/ProbeHandlerDto.java
@@ -4,12 +4,12 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 /**
- * Marker interface for probe handler DTOs. Only httpGet is supported currently; more types may be
- * added later (e.g. tcpSocket, exec, grpc).
+ * Marker interface for probe handler DTOs.
  */
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "$type")
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = HttpGetProbeDto.class, name = "httpGet")
+        @JsonSubTypes.Type(value = HttpGetProbeDto.class, name = "httpGet"),
+        @JsonSubTypes.Type(value = TcpSocketProbeDto.class, name = "tcpSocket")
 })
 public interface ProbeHandlerDto {
 }
