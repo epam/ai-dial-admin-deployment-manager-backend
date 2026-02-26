234c06505a32333e03d89dc4d187e53a5c0999e5
Vladyslav Pasichniuk <vladyslav_pasichniuk@epam.com>
2026-02-26 13:59:09 +0200

    Refactoring & minor improvements

--- src/main/java/com/epam/aidial/deployment/manager/configuration/ExecutorServiceConfiguration.java
+++ src/main/java/com/epam/aidial/deployment/manager/configuration/ExecutorServiceConfiguration.java
@@ -1,15 +1,12 @@
 package com.epam.aidial.deployment.manager.configuration;
 
-import org.jspecify.annotations.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Configuration
 public class ExecutorServiceConfiguration {
@@ -17,7 +14,10 @@ public class ExecutorServiceConfiguration {
     @Bean(name = "k8s-service-readiness-checker", destroyMethod = "shutdown")
     public ExecutorService k8sServiceReadinessCheckerExecutor(ReconciliationExecutorProperties properties) {
         var queue = new LinkedBlockingQueue<Runnable>(properties.getQueueCapacity());
-        var threadFactory = new ThreadFactoryWithPrefix(properties.getThreadNamePrefix());
+        var threadFactory = Thread.ofPlatform()
+                .name(properties.getThreadNamePrefix() + "-", 1)
+                .factory();
+
         return new ThreadPoolExecutor(
                 properties.getThreadPoolSize(),
                 properties.getThreadPoolSize(),
@@ -27,19 +27,5 @@ public class ExecutorServiceConfiguration {
                 threadFactory,
                 new ThreadPoolExecutor.CallerRunsPolicy()
         );
-    }
-
-    private static final class ThreadFactoryWithPrefix implements ThreadFactory {
-        private final String prefix;
-        private final AtomicInteger counter = new AtomicInteger(0);
-
-        ThreadFactoryWithPrefix(String prefix) {
-            this.prefix = prefix;
-        }
-
-        @Override
-        public Thread newThread(@NonNull Runnable r) {
-            return new Thread(r, prefix + "-" + counter.incrementAndGet());
-        }
     }
 }
