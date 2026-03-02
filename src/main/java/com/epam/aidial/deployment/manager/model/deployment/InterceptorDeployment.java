b2eb3bf24485aa2af3cd857dccdf0da4e492970e
Oleksii Donets <oleksii_donets@epam.com>
2026-03-02 18:14:30 +0200

    chore: fix tests

--- src/main/java/com/epam/aidial/deployment/manager/model/deployment/InterceptorDeployment.java
+++ src/main/java/com/epam/aidial/deployment/manager/model/deployment/InterceptorDeployment.java
@@ -3,6 +3,7 @@ package com.epam.aidial.deployment.manager.model.deployment;
 import com.epam.aidial.deployment.manager.model.Scaling;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import org.jetbrains.annotations.Nullable;
@@ -10,6 +11,7 @@ import org.jetbrains.annotations.Nullable;
 @Getter
 @Setter
 @SuperBuilder
+@NoArgsConstructor
 @AllArgsConstructor
 public class InterceptorDeployment extends Deployment {
     @Nullable
